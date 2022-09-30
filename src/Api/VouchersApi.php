cbd95a4c704a8610c8e27655ded0597bb35438ad
Azure DevOps <noreply@flipdish.com>
2022-09-30 10:43:42 +0100

    Auto generated

--- src/Api/VouchersApi.php
+++ src/Api/VouchersApi.php
@@ -1422,7 +1422,7 @@ class VouchersApi
      *
      * @param  int $voucher_id Id of the voucher (required)
      * @param  \Flipdish\\Client\Models\VoucherBase $voucher Updated details for the voucher (required)
-     * @param  int[] $store_id store_id (optional)
+     * @param  int[] $store_id List of store ids associated (optional)
      * @param  int $percent_value Percent voucher value (can have 1 of 3) (optional)
      * @param  double $lump_value Lump voucher value (can have 1 of 3) (optional)
      * @param  int $free_item_id Free Item Id (can have 1 of 3) (optional)
@@ -1444,7 +1444,7 @@ class VouchersApi
      *
      * @param  int $voucher_id Id of the voucher (required)
      * @param  \Flipdish\\Client\Models\VoucherBase $voucher Updated details for the voucher (required)
-     * @param  int[] $store_id (optional)
+     * @param  int[] $store_id List of store ids associated (optional)
      * @param  int $percent_value Percent voucher value (can have 1 of 3) (optional)
      * @param  double $lump_value Lump voucher value (can have 1 of 3) (optional)
      * @param  int $free_item_id Free Item Id (can have 1 of 3) (optional)
@@ -1556,7 +1556,7 @@ class VouchersApi
      *
      * @param  int $voucher_id Id of the voucher (required)
      * @param  \Flipdish\\Client\Models\VoucherBase $voucher Updated details for the voucher (required)
-     * @param  int[] $store_id (optional)
+     * @param  int[] $store_id List of store ids associated (optional)
      * @param  int $percent_value Percent voucher value (can have 1 of 3) (optional)
      * @param  double $lump_value Lump voucher value (can have 1 of 3) (optional)
      * @param  int $free_item_id Free Item Id (can have 1 of 3) (optional)
@@ -1581,7 +1581,7 @@ class VouchersApi
      *
      * @param  int $voucher_id Id of the voucher (required)
      * @param  \Flipdish\\Client\Models\VoucherBase $voucher Updated details for the voucher (required)
-     * @param  int[] $store_id (optional)
+     * @param  int[] $store_id List of store ids associated (optional)
      * @param  int $percent_value Percent voucher value (can have 1 of 3) (optional)
      * @param  double $lump_value Lump voucher value (can have 1 of 3) (optional)
      * @param  int $free_item_id Free Item Id (can have 1 of 3) (optional)
@@ -1636,7 +1636,7 @@ class VouchersApi
      *
      * @param  int $voucher_id Id of the voucher (required)
      * @param  \Flipdish\\Client\Models\VoucherBase $voucher Updated details for the voucher (required)
-     * @param  int[] $store_id (optional)
+     * @param  int[] $store_id List of store ids associated (optional)
      * @param  int $percent_value Percent voucher value (can have 1 of 3) (optional)
      * @param  double $lump_value Lump voucher value (can have 1 of 3) (optional)
      * @param  int $free_item_id Free Item Id (can have 1 of 3) (optional)
