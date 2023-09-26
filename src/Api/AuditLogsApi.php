ae1bdecdecb31fa33f52749ef468047818d98e8f
Azure DevOps <noreply@flipdish.com>
2023-09-26 11:20:11 +0000

    Auto generated

--- src/Api/AuditLogsApi.php
+++ src/Api/AuditLogsApi.php
@@ -149,14 +149,6 @@ class AuditLogsApi
 
         } catch (ApiException $e) {
             switch ($e->getCode()) {
-                case 400:
-                    $data = ObjectSerializer::deserialize(
-                        $e->getResponseBody(),
-                        '\Flipdish\\Client\Models\RestApiErrorResult',
-                        $e->getResponseHeaders()
-                    );
-                    $e->setResponseObject($data);
-                    break;
                 case 401:
                     $data = ObjectSerializer::deserialize(
                         $e->getResponseBody(),
